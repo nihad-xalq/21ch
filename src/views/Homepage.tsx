cbc1116d95a220a6a869b1586eabdddebca0be89
Nihad Abbasov <nihad.abbasov.work@gmail.com>
2025-10-15 00:13:27 +0400

    videos grid from 5 to 4

--- src/views/Homepage.tsx
+++ src/views/Homepage.tsx
@@ -60,7 +60,7 @@ const Homepage = () => {
                 2025 Collections videos
               </p> */}
             </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-8">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
               {videosArray.map((video, index) => (
                 <motion.div
                   key={video.id}
